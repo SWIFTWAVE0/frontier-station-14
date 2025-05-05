db0391dd38d6e1ddccd862081404ab243f27e852
xDanilcusx <xDanilcusx@gmail.com>
2025-05-05 17:33:49 +0300

    ломать камушки без смс и регистрации

--- Content.Server/Gatherable/GatherableSystem.Projectile.cs
+++ Content.Server/Gatherable/GatherableSystem.Projectile.cs
@@ -2,11 +2,16 @@ using Content.Server.Gatherable.Components;
 using Content.Shared.Mining.Components;
 using Content.Shared.Projectiles;
 using Robust.Shared.Physics.Events;
+using Robust.Shared.Map;
+using Robust.Shared.Map.Components;
+using Content.Shared.Coordinates;
 
 namespace Content.Server.Gatherable;
 
 public sealed partial class GatherableSystem
 {
+    [Dependency] private readonly IEntityManager _entManager = default!;
+    [Dependency] private readonly SharedMapSystem _mapSystem = default!;
     private void InitializeProjectile()
     {
         SubscribeLocalEvent<GatheringProjectileComponent, StartCollideEvent>(OnProjectileCollide);
@@ -22,6 +27,17 @@ public sealed partial class GatherableSystem
             return;
         }
 
+        // DAN CHANGES START
+
+        EntityUid? gridId = _entManager.GetComponent<TransformComponent>(args.OtherEntity).GridUid;
+        if (_entManager.TryGetComponent(gridId, out MapGridComponent? grid))
+        {
+            var childTransform = _entManager.GetComponent<TransformComponent>(args.OtherEntity);
+            _mapSystem.SetTile(gridId.Value, grid, childTransform.Coordinates, Tile.Empty);
+        }
+
+        // DAN CHANGES END
+
         // Frontier: gathering changes
         // bad gatherer - not strong enough
         if (_whitelistSystem.IsWhitelistFail(gatherable.ToolWhitelist, gathering.Owner))
@@ -29,12 +45,14 @@ public sealed partial class GatherableSystem
             QueueDel(gathering);
             return;
         }
+        /* DAN CHANGES START
         // Too strong (e.g. overpen) - gathers ore but destroys it
         if (TryComp<OreVeinComponent>(args.OtherEntity, out var oreVein)
             && _whitelistSystem.IsWhitelistPass(oreVein.GatherDestructionWhitelist, gathering.Owner))
         {
             oreVein.PreventSpawning = true;
         }
+        DAN CHANGES END */
         // End Frontier: gathering changes
 
         Gather(args.OtherEntity, gathering, gatherable);
